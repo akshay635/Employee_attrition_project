be399a67e611208e761b105de6b049f5bdd8cc26
Akshay Atanure <akshayatanure11@gmail.com>
2026-01-19 12:15:29 +0530

    Update attrition.py

--- employee_attrition_project/attrition.py
+++ employee_attrition_project/attrition.py
@@ -176,13 +176,13 @@ if st.button('Predict'):
             predict = model_rf.predict(df)
             predict_proba = model_rf.predict_proba(df)[0, 1]
             if predict_proba < 0.35:
-                st.success(f'{":white_check_mark:"} Employee is likely to stay with a low attrition risk score of {predict_proba:.2%}')
+                st.success(f'✅ Employee is likely to stay with a low attrition risk score of {predict_proba:.2%}')
                 st.write(f'Attrition rate: {predict_proba:.2%}')
             elif predict_proba >= 0.35 and predict_proba < 0.65:
-                st.warning(f'{":warning:"} Employee has a moderate risk of leaving with a score of {predict_proba:.2%}')
+                st.warning(f'⚠️ Employee has a moderate risk of leaving with a score of {predict_proba:.2%}')
                 st.write(f'Attrition rate: {predict_proba:.2%}')
             else:
-                st.error(f'{:x:} Employee is at high risk of leaving with a probability of {predict_proba:.2%}')
+                st.error(f'❌ Employee is at high risk of leaving with a probability of {predict_proba:.2%}')
                 st.write(f'Attrition rate: {predict_proba:.2%}')
     
             st.text("SHAP explanations")
@@ -200,6 +200,7 @@ if st.button('Predict'):
         
 
         
+
 
 
 
