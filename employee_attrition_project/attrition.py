d341b83eca0a5b5cdc1ed71297613fa432e7d702
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-13 23:07:56 +0530

    Update attrition.py

--- employee_attrition_project/attrition.py
+++ employee_attrition_project/attrition.py
@@ -96,7 +96,7 @@ if st.button('Predict'):
         )
 
         fig, ax = plt.subplots()
-        shap.plots.bar(collapsed_explanation[:, 1], max_display=10)
+        shap.plots.waterfall(collapsed_explanation[:, 1], max_display=10)
         st.pyplot(fig, use_container_width=True)
         st.markdown("""Features shown in red increase the employee’s likelihood of leaving the organization, 
                        while features shown in blue increase the likelihood of the employee staying.""")
@@ -117,6 +117,7 @@ if st.button('Predict'):
 
 
         
+
 
 
 
