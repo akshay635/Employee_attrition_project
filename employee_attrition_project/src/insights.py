eccaf070800e0b9392ec0b54765a3849c12c1fd5
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-13 12:27:05 +0530

    Update insights.py

--- employee_attrition_project/src/insights.py
+++ employee_attrition_project/src/insights.py
@@ -4,8 +4,6 @@ import shap
 
 def generate_feature_insight(importances, top_n = 5):
     top_features = importances.sort_values(by='importance', ascending=False).head(top_n)
-    lines = [f"### 🧠 Top {top_n} Feature Insights\n"]
-
     return top_features
 
     
