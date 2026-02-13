3588a97fdd5af72e6b50bc5f510ccf22b00cb49d
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-13 14:04:30 +0530

    Update model_explanation.py

--- employee_attrition_project/src/model_explanation.py
+++ employee_attrition_project/src/model_explanation.py
@@ -44,7 +44,7 @@ def collapse_shap_values(shap_values, encoded_feature_names, original_features):
     # Collapse each categorical feature
     for i, feature in enumerate(original_features):
         # Get encoded columns for this feature
-        encoded_cols = [col for col in encoded_feature_names if col.startswith('cat' + "_" + feature)]
+        encoded_cols = [col for col in encoded_feature_names if col.startswith('cat' + "__")]
         if encoded_cols:
             shap_df[feature] = shap_df[encoded_cols].sum(axis=1)
             shap_df.drop(columns=encoded_cols, inplace=True)
@@ -70,6 +70,7 @@ def SHAP_explanations(model, df):
     return shap_values, encoded_features
 
     
+
 
 
 
