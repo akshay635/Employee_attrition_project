9a704eca45bab0a1ac2d940ccaba22de4e07fe09
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-13 13:49:46 +0530

    Update model_explanation.py

--- employee_attrition_project/src/model_explanation.py
+++ employee_attrition_project/src/model_explanation.py
@@ -66,10 +66,9 @@ def SHAP_explanations(model, df):
     st.pyplot(fig, use_container_width=True)
     st.markdown("""Features shown in red increase the employee’s likelihood of leaving the organization, 
                    while features shown in blue increase the likelihood of the employee staying.""")
-    with st.expander('Feature explanations using SHAP'):
-        st.dataframe(collapse_shap_values(shap_values, encoded_features, config.COMMON_FEATURES))
 
     
+
 
 
 
