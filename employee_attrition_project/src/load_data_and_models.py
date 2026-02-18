3f37bc4818a6e26b3ee5c683476d99192e02ee4b
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-18 10:03:59 +0530

    Update data loading functions for new model paths

--- employee_attrition_project/src/load_data_and_models.py
+++ employee_attrition_project/src/load_data_and_models.py
@@ -7,7 +7,7 @@ import joblib
 #Load csv data
 @st.cache_data
 def load_data():
-    features_rf = pd.read_csv('employee_attrition_project/data/feature_importances_rf.csv')
+    features_rf = pd.read_csv('employee_attrition_project/data/forest_importances.csv')
     features_lg = pd.read_csv('employee_attrition_project/data/feature_importances_lg.csv')
     features_dt = pd.read_csv('employee_attrition_project/data/feature_importances_dt.csv')
     features_cat = pd.read_csv('employee_attrition_project/data/feature_importances_cat.csv')
@@ -19,9 +19,10 @@ def load_data():
 @st.cache_resource
 def load_models():
     model_lg = joblib.load('employee_attrition_project/models/lg_attrition.joblib')
-    model_rf = joblib.load('employee_attrition_project/models/rf_attrition.joblib')
+    model_rf = joblib.load('employee_attrition_project/models/current_model.joblib')
     model_dt = joblib.load('employee_attrition_project/models/dt_attrition.joblib')
     model_cat = joblib.load('employee_attrition_project/models/cat_attrition.joblib')
     
     return model_lg, model_rf, model_dt, model_cat
+
 
