cdcf60871a80c461176a901b6f1173889cba5c1f
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-18 10:09:24 +0530

    Fix sorting column name in generate_feature_insight

--- employee_attrition_project/src/insights.py
+++ employee_attrition_project/src/insights.py
@@ -3,7 +3,7 @@ import numpy as np
 import shap
 
 def generate_feature_insight(importances, top_n = 5):
-    top_features = importances.sort_values(by='importance', ascending=False).head(top_n)
+    top_features = importances.sort_values(by='Importances', ascending=False).head(top_n)
     return top_features
 
     
