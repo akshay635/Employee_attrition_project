01499906bf5f2d5859430b19a773f8f8992d50c8
Akshay Atanure <akshayatanure11@gmail.com>
2026-03-05 11:35:22 +0530

    Refactor load_data and load_models functions
    Simplified data loading functions to only load random forest features and model.

--- employee_attrition_project/src/load_data_and_models.py
+++ employee_attrition_project/src/load_data_and_models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import streamlit as st
 import pandas as pd
 import joblib
@@ -8,21 +7,15 @@ import joblib
 @st.cache_data
 def load_data():
     features_rf = pd.read_csv('employee_attrition_project/data/forest_importances.csv')
-    features_lg = pd.read_csv('employee_attrition_project/data/feature_importances_lg.csv')
-    features_dt = pd.read_csv('employee_attrition_project/data/feature_importances_dt.csv')
-    features_cat = pd.read_csv('employee_attrition_project/data/feature_importances_cat.csv')
     
-    return features_lg, features_rf, features_dt, features_cat
-
+    return features_rf
 
 # Load trained pipeline
 @st.cache_resource
 def load_models():
-    model_lg = joblib.load('employee_attrition_project/models/lg_attrition.joblib')
     model_rf = joblib.load('employee_attrition_project/models/current_model.joblib')
-    model_dt = joblib.load('employee_attrition_project/models/dt_attrition.joblib')
-    model_cat = joblib.load('employee_attrition_project/models/cat_attrition.joblib')
     
-    return model_lg, model_rf, model_dt, model_cat
+    return model_rf
+
 
 
